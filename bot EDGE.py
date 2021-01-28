c4101c1ca41964e0bdab864d0f5702a0ef26bc8f
RealJakeTheDog <39779292+RealJakeTheDog@users.noreply.github.com>
2021-01-28 00:50:10 -0600

    Updates/Additions
    Added db!insult and db!complement (Working)
    Added Lego Yoda Death Sound Lottery (Not Working)

--- bot EDGE.py
+++ bot EDGE.py
@@ -1,5 +1,12 @@
-#importing the discord module
+#Import
 import discord
+import random
+from time import sleep
+
+#Reset Variables
+Insult = 0
+Lottery = 0
+
 
 #creating a new discord client
 client=discord.Client()
@@ -7,7 +14,7 @@ client=discord.Client()
 #methods waiting for the event
 @client.event
 
-#when the bot started running, we may print its name, id etc
+#Prints Username/User ID after starting
 async def on_ready():
     print('Logged in')
     print("Username: ",end='')
@@ -16,8 +23,13 @@ async def on_ready():
     print(client.user.id)
 @client.event
 
+#_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
 #when the bot gets the message this method gets triggered
 async def on_message(message):
+    Insult = 0
+    sleep(1)
+    Insult = (random.randint(1,10))
     if message.author.id == client.user.id:
         return
     #test command
@@ -39,23 +51,71 @@ async def on_message(message):
         db!derpbot: a small intro to the bot, including the invite link
         db!say [Text]: Makes the bot say something, replace [Text] with whatever you would like
         db!despacito: despacito
-        db!help: ...```''')
-        if message.content.startswith('db!insult'):
-            random = random.randint (1,10)
-            if random = 1 :
-                await message.channel.send('ORDINARILY PEOPLE LIVE AND LEARN. YOU JUST LIVE.')
-            elif random = 2 :
-                await message.channel.send('YOU\'RE NOT FUNNY, BUT YOUR LIFE, NOW THAT\'S A JOKE.')
-            elif random = 3 :
-                await message.channel.send('YOU\'RE AS USELESS AS A COMPUTER WITHOUT INTERNET.')
-            elif random = 4 :
-                await message.channel.send('YOUR ASININE SIMIAN COUNTENANCE ALLUDES THAT YOUR FETID STENCH HAS ANULLED THE ANTHROPOID APE SPECIES DIVERSITY.')
-            elif random = 5 :
-                await message.channel.send('YOU\'RE NOT PRETTY ENOUGH TO BE THIS STUPID.')
-            elif random = 6 :
-                await message.channel.send('YOU\'RE SO STUPID, IT TAKES YOU AN HOUR TO COOK MINUTE RICE.')
+        db!help: ...
+        db!insult: Take A Random Guess
+        db!compliment: A bunch of backhanded compliments```''')
+    #Insult Command
+    if message.content.startswith('db!insult'):
+        if Insult == 1 :
+            await message.channel.send('ORDINARILY PEOPLE LIVE AND LEARN. YOU JUST LIVE.')
+        elif Insult == 2 :
+            await message.channel.send('YOU\'RE NOT FUNNY, BUT YOUR LIFE, NOW THAT\'S A JOKE.')
+        elif Insult == 3 :
+            await message.channel.send('YOU\'RE AS USELESS AS A COMPUTER WITHOUT INTERNET.')
+        elif Insult == 4 :
+            await message.channel.send('YOUR ASININE SIMIAN COUNTENANCE ALLUDES THAT YOUR FETID STENCH HAS ANULLED THE ANTHROPOID APE SPECIES DIVERSITY.')
+        elif Insult == 5 :
+            await message.channel.send('YOU\'RE NOT PRETTY ENOUGH TO BE THIS STUPID.')
+        elif Insult == 6 :
+            await message.channel.send('YOU\'RE SO STUPID, IT TAKES YOU AN HOUR TO COOK MINUTE RICE.')
+        elif Insult == 7 :
+            await message.channel.send('WHEN YOU WERE BORN, THEY THREW AWAY THE BABY AND KEPT THE PLACENTA...')
+        elif Insult == 8 :
+            await message.channel.send('SOMEDAY YOUR FRUSTRATIONS WITH OTHER PEOPLE WILL END, BUT ONLY BECAUSE YOU WILL DIE ALONE.')
+        elif Insult == 9 :
+            await message.channel.send('EVERYONE WHO EVER LOVED YOU WAS WRONG')
+        elif Insult == 10 :
+            await message.channel.send('I BET YOUR BRAIN FEELS AS GOOD AS NEW, SEEING THAT YOU NEVER USE IT.')
+    #Compliment Command
+    if message.content.startswith('db!compliment'):
+        if Insult == 1 :
+            await message.channel.send('The FBI tapped your phone just to hear the sound of your voice.')
+        if Insult == 2 :
+            await message.channel.send('People behind you at movies think you are the perfect height.')
+        if Insult == 3 :
+            await message.channel.send('There was a high school rumor that you are a distant relative of Abraham Lincoln.')
+        if Insult == 4 :
+            await message.channel.send('Callers are intimidated by how funny your voicemail greeting is.')
+        if Insult == 5 :
+            await message.channel.send('80% of motorcycle gangs think you’d be a delightful sidecar.')
+        if Insult == 6 :
+            await message.channel.send('You are your parent\'s greatest accomplishment, unless they invented the \"spork\".')
+        if Insult == 7 :
+            await message.channel.send('You never forget to fill the ice-cube tray.')
+        if Insult == 8 :
+            await message.channel.send('Your allergies are some of the least embarrassing allergies.')
+        if Insult == 9 :
+            await message.channel.send('Some dudes hope you start a band so they can start a cover band of that band.')
+        if Insult == 10 :
+            await message.channel.send('Kids think you are the “cool old person”.')
+#___________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
+#Lego Yoda Death Sound Lottery
+async def on_voice_server_update : #Updates Code Every Time A User Leaves Or Joins
+    if user.id(arg) == 1 : #Defines How Many People Are In A VC At Once. Updates Every Time A User Leaves Or Joins
+        while (Lottery != 20):
+            Lottery = (random.randint(19,20)) #Random Number Between _-_, Defines Chances of bot joining VC
+            print str(Lottery)
+            sleep(10) #Waits X Amount Of Seconds Before Picking Another Number
+        async def join(ctx):
+            voice_channel = user.channel #Joins Channel That User Is In
+            vc = await VoiceChannel.connect(voice_channel)
+            sleep(1)
+            vc.play(discord.FFmpegPCMAudio('LYDS.mp3'), after=lamda: print('done')) #Plays .mp3 File
+            if is_playing():
+                return
+            elif stop():
+                vc = await disconnect
+                
 # Bot Token/Run
-client.run("ODA0MTUwMzYzNjAyNDE5Nzgy.YBIJYw.LIJrEBtPKgqTP1IwSEsH4bgYt5s")
-# Main Bot Token: ODA0MDM2MTAzNDA5MzY5MDk4.YBGe-Q.CxUgdR4lOfp7os_aVlzFZB3Erhc
-# Edge Bot Token: ODA0MTUwMzYzNjAyNDE5Nzgy.YBIJYw.LIJrEBtPKgqTP1IwSEsH4bgYt5s
+client.run("****************")
