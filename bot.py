3b79e3cb4da7202fabc6a7680d911c9eaf14a860
RealJakeTheDog <39779292+RealJakeTheDog@users.noreply.github.com>
2021-01-30 20:53:36 -0600

    Updates/Optimizations
    Added db!FlipACoin, db!doing , and GG!EnD01230
    db!FlipACoin - Flips a coin by using the coin variable
    db!doing - Changes The Status of the bot
    GG!EnD01230 - Terminates the bot
    The reason the Terminate command is so random is so that random people just spamming commands looking for secret commands can't accidentally kill the bot

--- bot.py
+++ bot.py
@@ -1,15 +1,26 @@
 #Import
 import discord
 import random
+import asyncio
+import discord.ext
+import discord.voice_client
+from discord.ext import commands
+from discord.ext.commands import bot
 from time import sleep
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 #Reset Variables
-Insult = 0
-Lottery = 0
+Insult = 0 #Insult/Compliment Variable
+Lottery = 0 #Not Used Yet
+coin = 0 #Coin Flip Variable
+members = "test" #Not Used Yet
+
 
 
 #creating a new discord client
 client=discord.Client()
+bot = commands.Bot(command_prefix='db!')
 
 #methods waiting for the event
 @client.event
@@ -21,6 +32,7 @@ async def on_ready():
     print(client.user.name)
     print("Userid: ",end='')
     print(client.user.id)
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("db!help"))
 @client.event
 
 #_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -28,13 +40,15 @@ async def on_ready():
 #when the bot gets the message this method gets triggered
 async def on_message(message):
     Insult = 0
-    sleep(1)
+    coin = 0
+    sleep(0.3)
     Insult = (random.randint(1,10))
+    coin = (random.randint(1,3))
     if message.author.id == client.user.id:
         return
     #test command
     if message.content.startswith('db!test'):
-        await message.channel.send('This Command Is Useless'.format(message))
+        await message.channel.send('Hello '+ (message.author.mention))
     #derpbot command
     elif message.content.startswith('db!derpbot'):
         await message.channel.send('I am the reincarnation of the once powerful Derpbot (Invite link: https://bit.ly/2YmxOfw)')
@@ -43,17 +57,19 @@ async def on_message(message):
         await message.channel.send(message.content.replace('db!say ',''))
     #Despacito Command
     elif message.content.startswith('db!despacito'):
-        await message.channel.send('```Despacito```' + '{0.author.mention}')
+        await message.channel.send('Did some carbon based life form just say \*\*\*DESPACITO\*\*\*')
     #Help Command
     elif message.content.startswith('db!help'):
         await message.channel.send('''```
         db!test: A way to check if the bot is actually online
         db!derpbot: a small intro to the bot, including the invite link
         db!say [Text]: Makes the bot say something, replace [Text] with whatever you would like
-        db!despacito: despacito
+        db!despacito: Despacito
         db!help: ...
         db!insult: Take A Random Guess
-        db!compliment: A bunch of backhanded compliments```''')
+        db!compliment: A bunch of backhanded compliments
+        db!FlipACoin: Flips A Coin
+        db!doing: Changes the bot's status```''')
     #Insult Command
     if message.content.startswith('db!insult'):
         if Insult == 1 :
@@ -97,6 +113,25 @@ async def on_message(message):
         if Insult == 9 :
             await message.channel.send('Some dudes hope you start a band so they can start a cover band of that band.')
         if Insult == 10 :
-            await message.channel.send('Kids think you are the “cool old person”.')
+            await message.channel.send('Kids think you are the \“cool old person\”.')
+    #Coin Flip Command
+    if message.content.startswith('db!FlipACoin'):
+        if coin == 1 :
+            await message.channel.send('It Was Heads')
+        if coin == 2 :
+            await message.channel.send('It Was Tails')
+        elif coin == 3 :
+            await message.channel.send('The Coin Landed On Its Side')
+    #Terminate Command
+    if message.content.startswith('GG!EnD01230'):
+        await message.channel.send('Goodbye')
+        await client.change_presence(status=discord.Status.invisible)
+        sleep(1)
+        await client.close()
+    #Status Command
+    if message.content.startswith('db!doing'):
+        await client.change_presence(status=discord.Status.online, activity=discord.Game(message.content.replace('db!doing','')))
+        await message.channel.send("Doing ur mom")
+#______________________________________________________________________________________________________________________________________________________________________________
   # Bot Token/Run
 client.run("*************")
